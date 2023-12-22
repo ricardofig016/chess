18bfef87f400c9728e29736b1ee48a5b15c82642
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-12-22 00:07:57 +0000

    window with board and pieces

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os, pygame, tkinter
 from classes.board import Board
 
+white = (213, 222, 229)
+blue = (117, 153, 174)
+black = (0, 0, 0)
+
 
 def get_screen_resolution():
     root = tkinter.Tk()
@@ -13,15 +17,50 @@ def get_screen_resolution():
     return screen_width, screen_height
 
 
+def draw_board(screen, height):
+    for row in range(8):
+        for col in range(8):
+            color = white if (row + col) % 2 == 0 else blue
+            pygame.draw.rect(
+                screen,
+                color,
+                (
+                    col * height * 0.125,
+                    row * height * 0.125,
+                    height * 0.125,
+                    height * 0.125,
+                ),
+            )
+    return
+
+
+def get_piece_img(piece, assets_path, size):
+    name = piece.name
+    color = piece.color
+    file_name = color + "-" + name + ".png"
+    return pygame.transform.scale(
+        pygame.image.load(os.path.join(assets_path, file_name)), (size, size)
+    )
+
+
+def blit_pieces(screen, board, assets_path, height):
+    for row in range(8):
+        for col in range(8):
+            piece = board.matrix[row][col].piece
+            if piece:
+                img = get_piece_img(piece, assets_path, height * 0.125)
+                screen.blit(img, (height * 0.125 * col, height * 0.125 * row))
+    return
+
+
 def window(width, height, assets_path):
+    board = Board()
+
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     # pygame.display.toggle_fullscreen()
     pygame.display.set_caption("Chess")
     clock = pygame.time.Clock()
-    background = pygame.transform.scale(
-        pygame.image.load(os.path.join(assets_path, "board.jpg")), (width, height)
-    )
 
     running = True
     while running:
@@ -32,7 +71,10 @@ def window(width, height, assets_path):
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.blit(background, (0, 0))
+        screen.fill(white)
+        draw_board(screen, height)
+        blit_pieces(screen, board, assets_path, height)
+        pygame.draw.line(screen, black, (height, 0), (height, height), 2)
 
         pygame.display.update()
         clock.tick(60)
@@ -43,12 +85,11 @@ def window(width, height, assets_path):
 
 def run():
     screen_width, screen_height = get_screen_resolution()
-    width, height = 600, 600
-
+    width = int(screen_width * 0.8)
+    height = int(width * 0.5625)
+    # print(width, height)
     assets_path = "assets/"
     window(width, height, assets_path)
-    board = Board()
-    print(board)
     return
 
 
